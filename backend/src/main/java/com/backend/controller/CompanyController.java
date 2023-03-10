74328f0f529b61d676d3c73faa3dc74d9cd1f2d0
alsk1369854 <alsk1369854@gmail.com>
2023-03-10 18:01:15 +0800

    [update] company CURD

--- backend/src/main/java/com/backend/controller/CompanyController.java
+++ backend/src/main/java/com/backend/controller/CompanyController.java
@@ -31,4 +31,25 @@ public class CompanyController {
         return companyRepository.findById(id)
                 .orElseThrow(()-> new CompanyNotFoundException(id));
     }
+
+    @PutMapping("/company/{id}")
+    Company updateCompany(@RequestBody Company newCompany, @PathVariable Long id){
+        return companyRepository.findById(id)
+                .map(company ->{
+                    company.setName(newCompany.getName());
+                    company.setArrears(newCompany.getArrears());
+                    company.setRemark(newCompany.getRemark());
+                    return companyRepository.save(company);
+                }).orElseThrow(() -> new CompanyNotFoundException(id));
+
+    }
+
+    @DeleteMapping("/company/{id}")
+    String deleteCompany(@PathVariable Long id){
+        if(!companyRepository.existsById(id)){
+            throw new CompanyNotFoundException(id);
+        }
+        companyRepository.deleteById(id);
+        return "Company with id: " + id + " has been deleted success.";
+    }
 }
