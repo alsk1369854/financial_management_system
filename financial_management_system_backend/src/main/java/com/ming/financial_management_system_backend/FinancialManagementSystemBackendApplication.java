b9fafcfb89a3bcb5a4e057aeacddf2c61420d50b
alsk1369854 <alsk1369854@gmail.com>
2023-05-18 21:46:54 +0800

    update

--- financial_management_system_backend/src/main/java/com/ming/financial_management_system_backend/FinancialManagementSystemBackendApplication.java
+++ financial_management_system_backend/src/main/java/com/ming/financial_management_system_backend/FinancialManagementSystemBackendApplication.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+
 @OpenAPIDefinition(info = @Info(title = "Financial Management System APIs", version = "1.0.0"))
+@ServletComponentScan
 @SpringBootApplication
 public class FinancialManagementSystemBackendApplication {
 
---
+++ financial_management_system_backend/src/main/java/com/ming/financial_management_system_backend/filters/BaseFilter.java
@@ -0,0 +1,19 @@
+package com.ming.financial_management_system_backend.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@WebFilter(urlPatterns = "/*")
+public class BaseFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+}
